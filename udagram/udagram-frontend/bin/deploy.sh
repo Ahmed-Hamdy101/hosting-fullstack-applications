8d6c29f46ec57b21d7852251106264ebc7093b1a
Turria101 <94504141+Turria101@users.noreply.github.com>
2022-11-17 07:22:27 +0200

    update

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,17 +1,20 @@
 aws s3 cp --recursive --acl public-read ./www s3://my-746193541454-bucket/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://my-746193541454-bucket
  
- eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID &&
- eb setenv AWS_BUCKET=$AWS_BUCKET &&
- eb setenv AWS_REGION=$AWS_REGION &&
- eb setenv AWS_PROFILE=$AWS_PROFILE &&
- eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY &&
- eb setenv DB_PORT=$DB_PORT &&
- eb setenv PORT=$PORT &&
- eb setenv JWT_SECRET=$JWT_SECRET &&
- eb setenv POSTGRES_DB=$POSTGRES_DB &&
- eb setenv POSTGRES_HOST=$POSTGRES_HOST &&
- eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD &&
- eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME &&
- eb setenv URL=$URL && 
+#  eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID &&
+#  eb setenv AWS_BUCKET=$AWS_BUCKET &&
+#  eb setenv AWS_REGION=$AWS_REGION &&
+#  eb setenv AWS_PROFILE=$AWS_PROFILE &&
+#  eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY &&
+#  eb setenv DB_PORT=$DB_PORT &&
+#  eb setenv PORT=$PORT &&
+#  eb setenv JWT_SECRET=$JWT_SECRET &&
+#  eb setenv POSTGRES_DB=$POSTGRES_DB &&
+#  eb setenv POSTGRES_HOST=$POSTGRES_HOST &&
+#  eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD &&
+#  eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME &&
+#  eb setenv URL=$URL && 
  eb use udagram-api-dev
+ eb deploy udagram-api-dev
+ # or 
+ #cd ../../udagram-api && npm run env
