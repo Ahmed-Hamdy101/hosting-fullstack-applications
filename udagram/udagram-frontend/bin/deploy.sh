aaefa9cfd86d67a8dda3e7b898976a87fe37449b
Turria101 <94504141+Turria101@users.noreply.github.com>
2022-11-17 05:24:14 +0200

    update

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,5 +1,5 @@
 aws s3 cp --recursive --acl public-read ./www s3://my-746193541454-bucket/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://my-746193541454-bucket/.elasticbeanstalk
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://my-746193541454-bucket
  
  eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID &&
  eb setenv AWS_BUCKET=$AWS_BUCKET &&
