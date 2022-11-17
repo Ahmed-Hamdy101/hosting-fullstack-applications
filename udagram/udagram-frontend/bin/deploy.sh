f4f3646e9ce95f47ba0d3be6bdf6a6430b818b8f
Turria101 <94504141+Turria101@users.noreply.github.com>
2022-11-17 07:49:39 +0200

    Update delpoy.sh

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,6 +1,5 @@
 aws s3 cp --recursive --acl public-read ./www s3://my-746193541454-bucket/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://my-746193541454-bucket/
- 
+
 #  eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID &&
 #  eb setenv AWS_BUCKET=$AWS_BUCKET &&
 #  eb setenv AWS_REGION=$AWS_REGION &&
@@ -14,7 +13,7 @@ aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must
 #  eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD &&
 #  eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME &&
 #  eb setenv URL=$URL && 
- eb use udagram-api-dev
- eb deploy udagram-api-dev
+ #eb use udagram-api-dev
+ #eb deploy udagram-api-dev
  # or 
  #cd ../../udagram-api && npm run env
